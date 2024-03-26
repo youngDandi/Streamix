951e8ca3dc279537176d5bf401187715c473c3c5
youngDandi <101838296+youngDandi@users.noreply.github.com>
2024-03-26 22:36:35 +0100

    Add files via upload
    first commit

---
+++ streamix/src/FrontEnd/View/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+import MenuDiv from "../Components/MenuDiv/MenuDiv";
+import Display from "../Components/Display/Display";
+
+function Home() {
+  return (
+    <div className='todaPagina'>
+        <MenuDiv></MenuDiv>
+        
+        
+    </div>
+  );
+}
+
+export default Home;
