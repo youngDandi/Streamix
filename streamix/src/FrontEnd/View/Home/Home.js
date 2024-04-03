74bfa726aa1825bc8741901d86ccb9057eab6564
youngDandi <101838296+youngDandi@users.noreply.github.com>
2024-04-03 12:54:01 +0100

    alterações

---
+++ streamix/src/FrontEnd/View/Home/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+import MenuDiv from "../../Components/MenuDiv/MenuDiv";
+
+
+function Home() {
+  return (
+    <div className='todaPagina'>
+        <MenuDiv></MenuDiv>
+        
+        
+    </div>
+  );
+}
+
+export default Home;
---
+++ streamix/src/FrontEnd/View/Video/Video.js
@@ -0,0 +1,19 @@
+import './Video.css';
+import React, { useState, useEffect } from 'react';
+import MenuDiv from "../../Components/MenuDiv/MenuDiv";
+
+
+
+function Video() {
+
+  
+
+  return (
+    <div className='todaPaginaV'>
+        <MenuDiv></MenuDiv>
+  
+    </div>
+  );
+}
+
+export default Video;
